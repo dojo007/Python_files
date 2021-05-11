9a4b4a2eba6e14c03b381ed95344b300cf92cccc
JRuano <68176484+dojo007@users.noreply.github.com>
2021-05-11 12:54:42 -0700

    Add files via upload

---
+++ dictionare.py
@@ -0,0 +1,7 @@
+####This nested sample of dictionary is very simplistic . Although it only includes few lines of code is pretty  much the core base of all dictionaries.
+
+nested_dict = { 'dict1': {'key_A': 'value_A'},
+                'dict2': {'key_B': 'value_B'}}
+
+print(nested_dict)
+result to print ####{'dict1': {'key_A': 'value_A'}, 'dict2': {'key_B': 'value_B'}}
